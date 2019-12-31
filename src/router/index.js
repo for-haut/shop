cb37549c640483e4ec37e49c46f5b09013f2e641
it <123456@qq.com>
2019-12-31 11:50:00 +0800

    登录功能已完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import login from '../components/login.vue'
+import home from '../components/home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: login },
+  { path: '/home', component: home }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂在路由守卫
+router.beforeEach((to, from, next) => {
+  // to 要访问的网页   from 从哪个路径跳转而来   next 一个函数 放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
